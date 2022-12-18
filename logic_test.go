2f2ba5d59ef32a35ec99bc0040dc44278302ce5b
earlgray <earlgray283@gmail.com>
2022-12-18 17:51:10 +0900

    feat(test): wikipedia の例

--- logic_test.go
+++ logic_test.go
@@ -12,6 +12,7 @@ var (
 	one  = 1
 )
 
+// example from https://ja.wikipedia.org/wiki/%E3%82%AF%E3%83%AF%E3%82%A4%E3%83%B3%E3%83%BB%E3%83%9E%E3%82%AF%E3%83%A9%E3%82%B9%E3%82%AD%E3%83%BC%E6%B3%95
 func Test_QuineMcCluskey(t *testing.T) {
 	in := [][]int{
 		{0, 0, 0, 0},
@@ -52,15 +53,30 @@ func Test_QuineMcCluskey(t *testing.T) {
 
 	fmt.Println("===in===")
 	printTruthTable2(in)
+	fmt.Println()
+
 	fmt.Println("===out===")
 	printTruthTable(out)
+	fmt.Println()
 
 	ans, err := QuineMcCluskey(in, out)
 	if err != nil {
 		t.Fatal(err)
 	}
+
 	fmt.Println("===optimized-significant-table===")
 	printSignificantTable(ans[0])
+	fmt.Println()
+
+	assert.Equal(t, len(ans), 1)
+	assert.EqualValues(t, []*int{nil, &one, &zero, &zero}, ans[0][0].Significant)
+	assert.EqualValues(t, []int{4, 12}, ans[0][0].IndexList)
+	assert.EqualValues(t, []*int{&one, &zero, nil, nil}, ans[0][1].Significant)
+	assert.EqualValues(t, []int{8, 9, 10, 11}, ans[0][1].IndexList)
+	assert.EqualValues(t, []*int{&one, nil, nil, &zero}, ans[0][2].Significant)
+	assert.EqualValues(t, []int{8, 10, 12, 14}, ans[0][2].IndexList)
+	assert.EqualValues(t, []*int{&one, nil, &one, nil}, ans[0][3].Significant)
+	assert.EqualValues(t, []int{10, 11, 14, 15}, ans[0][3].IndexList)
 }
 
 func Test_calcHammingDistance(t *testing.T) {
